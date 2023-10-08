20b8a8e9461244eaac06e1c4218b57808d2297a6
Mouaadyacine <146827267+Mouaadyacine@users.noreply.github.com>
2023-10-08 12:39:11 +0200

    task11

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+a=int(input("donner le 1er entier"))
+b=int(input("donner le 2eme entier"))
+if a>0 and b>0 or a<0 and b<0:
+    print(a,"et",b,":ont le meme signe")
+else:
+    print(a,"et",b,":ont deux signe differents")
